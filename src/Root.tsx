6ca6b69cbf58d4a489bcaca61fd964f89f4ae30e
Owca <278430@student.pwr.edu.pl>
2025-07-03 11:53:17 +0200

    refactor: Remove ProtectedRoute component from routing in Root

--- src/Root.tsx
+++ src/Root.tsx
@@ -4,17 +4,14 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
-import ProtectedRoute from "./ProtectedRoute";
 
 function Root() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route element={<ProtectedRoute />}>
-        <Route path="/home" element={<Home />} />
-        <Route path="/product/:id" element={<ProductDetail />} />
-      </Route>
+      <Route path="/home" element={<Home />} />
+      <Route path="/product/:id" element={<ProductDetail />} />
     </Routes>
   );
 }
